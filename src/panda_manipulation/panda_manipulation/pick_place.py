7904933db69983e990148764479e857484a6b966
omaranwarr <oanwar@csuchico.edu>
2025-12-18 15:32:57 -0800

    Add files via upload

--- src/panda_manipulation/panda_manipulation/pick_place.py
+++ src/panda_manipulation/panda_manipulation/pick_place.py
@@ -1,4 +1,8 @@
 import rclpy
+import time
+import csv
+import os
+from visualization_msgs.msg import Marker
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from rclpy.executors import MultiThreadedExecutor
@@ -40,13 +44,20 @@ def make_quaternion_from_euler(roll, pitch, yaw):
     return q
 
 
-# “Straight” gripper: keep default panda_link8 orientation (identity)
+# “Straight” gripper: keep default panda_link8 orientation (identity), however grasper was rotating.
+"""
 TOP_DOWN_Q = Quaternion()
 TOP_DOWN_Q.x = 0.0
-TOP_DOWN_Q.y = 0.0
+TOP_DOWN_Q.y = 1.0
 TOP_DOWN_Q.z = 0.0
-TOP_DOWN_Q.w = 1.0
+TOP_DOWN_Q.w = 0.0
+"""
 
+TOP_DOWN_Q = make_quaternion_from_euler(
+    roll=-math.pi,
+    pitch=0.0,
+    yaw=0.0
+)
 
 
 
@@ -98,6 +109,66 @@ class PickPlaceNode(Node):
 
         # Make sure we don't start multiple pipelines in parallel
         self.busy = False
+        
+        # Track which phase of planning/execution we are in
+        self.current_phase = ""
+        
+        self.marker_pub = self.create_publisher(Marker, "/timing_marker", 10)
+
+        
+        # ==== CSV logging setup ====
+        self.metrics_dir = os.path.expanduser("~/panda_ws/metrics")
+        os.makedirs(self.metrics_dir, exist_ok=True)
+        self.metrics_file = os.path.join(self.metrics_dir, "pickplace_metrics.csv")
+
+        # Create file with header if doesn't exist
+        if not os.path.exists(self.metrics_file):
+            with open(self.metrics_file, "w", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    "timestamp",
+                    "ik_time",
+                    "plan_hover",
+                    "plan_grasp",
+                    "plan_lift",
+                    "plan_place",
+                    "exec_hover",
+                    "exec_grasp",
+                    "exec_lift",
+                    "exec_place",
+                    "pipeline_total"
+                ])
+         
+         
+         
+    def publish_marker(self, text, x=0.3, y=-0.5, z=1.0):
+        marker = Marker()
+        marker.header.frame_id = "world"
+        marker.type = Marker.TEXT_VIEW_FACING
+        marker.action = Marker.ADD
+        marker.scale.z = 0.05
+        marker.color.r = 1.0
+        marker.color.g = 1.0
+        marker.color.b = 0.2
+        marker.color.a = 1.0
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.position.z = z
+        marker.text = text
+        self.marker_pub.publish(marker)
+    def print_metrics_table(self):
+        self.get_logger().info("\n======= PERFORMANCE SUMMARY =======")
+        self.get_logger().info(f" IK time:               {self.metrics['ik_time']:.4f} s")
+        self.get_logger().info(f" Plan hover:            {self.metrics['plan_hover']:.4f} s")
+        self.get_logger().info(f" Plan grasp:            {self.metrics['plan_grasp']:.4f} s")
+        self.get_logger().info(f" Plan lift:             {self.metrics['plan_lift']:.4f} s")
+        self.get_logger().info(f" Plan place:            {self.metrics['plan_place']:.4f} s")
+        self.get_logger().info(f" Exec hover:            {self.metrics['exec_hover']:.4f} s")
+        self.get_logger().info(f" Exec grasp:            {self.metrics['exec_grasp']:.4f} s")
+        self.get_logger().info(f" Exec lift:             {self.metrics['exec_lift']:.4f} s")
+        self.get_logger().info(f" Exec place:            {self.metrics['exec_place']:.4f} s")
+        self.get_logger().info(f" TOTAL pipeline time:   {self.metrics['pipeline_total']:.4f} s")
+        self.get_logger().info("===================================")
 
     # ------------------------------------------------------------------
     # Callbacks
@@ -142,6 +213,166 @@ class PickPlaceNode(Node):
     # ------------------------------------------------------------------
     # Core pipeline
     # ------------------------------------------------------------------
+    def compute_grasp_height(self, obj_z):
+        APPLE_RADIUS = 0.04
+        SAFETY_MARGIN = 0.015  # 1.5 cm above apple
+
+    # Example:
+    # shelf top = 0.20m
+    # apple radius = 0.04 → center at z = 0.24
+    # top point = 0.28
+    # grasp slightly above = 0.295
+        grasp_z = obj_z + APPLE_RADIUS + SAFETY_MARGIN
+        hover_z = grasp_z + 0.10    # 10 cm hover
+        return grasp_z, hover_z
+
+    def run_pick_place(self):
+        
+        if self.busy:
+            return
+        if self.latest_object_pose is None:
+            return
+        if self.current_joint_state is None:
+            return
+	
+        pipeline_start = time.time()
+	# Reset metrics storage
+        self.metrics = {
+	    "ik_time": 0,
+	    "plan_hover": 0,
+	    "plan_grasp": 0,
+	    "plan_lift": 0,
+	    "plan_place": 0,
+	    "exec_hover": 0,
+	    "exec_grasp": 0,
+	    "exec_lift": 0,
+	    "exec_place": 0
+	}
+        self.busy = True
+        obj = self.latest_object_pose
+        
+        
+
+
+        try:
+            pipeline_start = self.get_clock().now()
+        # Ensure gripper is open before any motion
+            self.open_gripper()
+            self.get_logger().info("Gripper opened before starting motions.")
+            
+            self.get_logger().info(
+            f"Starting pick & place pipeline for object at "
+            f"x={obj.pose.position.x:.3f}, y={obj.pose.position.y:.3f},z={obj.pose.position.z:.3f}")
+            grasp_z, hover_z = self.compute_grasp_height(obj.pose.position.z)
+        # ------------------------
+        # 1) HOVER ABOVE OBJECT
+        # ------------------------
+            self.current_phase = "hover"
+            hover_pose = PoseStamped()
+            hover_pose.header.frame_id = "world"
+            hover_pose.pose.position.x = obj.pose.position.x
+            hover_pose.pose.position.y = obj.pose.position.y
+            #hover_pose.pose.position.z = obj.pose.position.z + 0.12
+            hover_pose.pose.position.z = hover_z
+            hover_pose.pose.orientation = TOP_DOWN_Q
+
+            hover_names, hover_positions = self.compute_ik(hover_pose)
+            self.current_phase = "hover"
+            hover_plan = self.plan_to_joint_goal(hover_names, hover_positions)
+            self.execute_plan(hover_plan, "hover over object")
+
+        # ------------------------
+        # 2) MOVE DOWN TO GRASP
+        # ------------------------
+            grasp_pose = PoseStamped()
+            grasp_pose.header.frame_id = "world"
+            grasp_pose.pose.position.x = obj.pose.position.x
+            grasp_pose.pose.position.y = obj.pose.position.y
+            #grasp_pose.pose.position.z = obj.pose.position.z + 0.02
+            grasp_pose.pose.position.z = grasp_z
+            grasp_pose.pose.orientation = TOP_DOWN_Q
+
+            grasp_names, grasp_positions = self.compute_ik(grasp_pose)
+            self.current_phase = "grasp"
+            grasp_plan = self.plan_to_joint_goal(grasp_names, grasp_positions)
+            self.execute_plan(grasp_plan, "descend to grasp")
+
+        # Close gripper
+            self.close_gripper()
+
+        # ------------------------
+        # 3) LIFT BACK TO HOVER
+        # ------------------------
+            self.current_phase = "lift"
+            lift_plan = self.plan_to_joint_goal(hover_names, hover_positions)
+            self.execute_plan(lift_plan, "lift object")
+
+        # ------------------------
+        # 4) MOVE TO PLACE HOVER
+        # ------------------------
+            place_hover = PoseStamped()
+            place_hover.header.frame_id = "world"
+            place_hover.pose.position.x = 0.50
+            place_hover.pose.position.y = -0.30
+            place_hover.pose.position.z = 0.50
+            place_hover.pose.orientation = TOP_DOWN_Q
+
+            ph_names, ph_pos = self.compute_ik(place_hover)
+            ph_plan = self.plan_to_joint_goal(ph_names, ph_pos)
+            self.execute_plan(ph_plan, "move above bin")
+
+        # ------------------------
+        # 5) LOWER INTO BIN
+        # ------------------------
+            place_pose = PoseStamped()
+            place_pose.header.frame_id = "world"
+            place_pose.pose.position.x = 0.50
+            place_pose.pose.position.y = -0.30
+            place_pose.pose.position.z = 0.33
+            place_pose.pose.orientation = TOP_DOWN_Q
+
+            p_names, p_pos = self.compute_ik(place_pose)
+            p_plan = self.plan_to_joint_goal(p_names, p_pos)
+            self.execute_plan(p_plan, "lower into bin")
+
+            # Open gripper
+            self.open_gripper()
+
+        finally:
+            self.busy = False
+            
+        # ---- pipeline total time metric ----
+        pipeline_end = self.get_clock().now()
+        pipeline_total = (pipeline_end - pipeline_start).nanoseconds / 1e9
+        self.get_logger().info(
+            f"TOTAL pick-place pipeline time: {pipeline_total:.3f} seconds"
+        )
+        if hasattr(self, "metrics"):
+            self.metrics["pipeline_total"] = pipeline_total
+
+        
+        # Write to CSV
+        with open(self.metrics_file, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                time.time(),
+                self.metrics["ik_time"],
+                self.metrics["plan_hover"],
+                self.metrics["plan_grasp"],
+                self.metrics["plan_lift"],
+                self.metrics["plan_place"],
+                self.metrics["exec_hover"],
+                self.metrics["exec_grasp"],
+                self.metrics["exec_lift"],
+                self.metrics["exec_place"],
+                self.metrics["pipeline_total"]
+            ])
+        self.get_logger().info(f"Metrics saved → {self.metrics_file}")
+        self.print_metrics_table()
+
+
+    
+    """
     def run_pick_place(self):
         # prevent overlapping runs
         if self.busy:
@@ -166,7 +397,7 @@ class PickPlaceNode(Node):
             hover_pose.header.frame_id = "world"
             hover_pose.pose.position.x = obj.pose.position.x
             hover_pose.pose.position.y = obj.pose.position.y
-            hover_pose.pose.position.z = obj.pose.position.z + 0.25  # 10 cm above
+            hover_pose.pose.position.z = obj.pose.position.z + 0.10  # 10 cm above
             hover_pose.pose.orientation = TOP_DOWN_Q
 
             self.get_logger().info("Computing IK for HOVER pose...")
@@ -189,7 +420,7 @@ class PickPlaceNode(Node):
             grasp_pose.header.frame_id = "world"
             grasp_pose.pose.position.x = obj.pose.position.x
             grasp_pose.pose.position.y = obj.pose.position.y
-            grasp_pose.pose.position.z = obj.pose.position.z + 0.05 # same as YOLO pose
+            grasp_pose.pose.position.z = obj.pose.position.z + 0.02 # same as YOLO pose
             grasp_pose.pose.orientation = TOP_DOWN_Q
 
 
@@ -223,7 +454,7 @@ class PickPlaceNode(Node):
             place_hover_pose.header.frame_id = "world"
             place_hover_pose.pose.position.x = 0.5
             place_hover_pose.pose.position.y = -0.3
-            place_hover_pose.pose.position.z = 0.70   # high above both shelf & bin
+            place_hover_pose.pose.position.z = 0.50   # high above both shelf & bin
             place_hover_pose.pose.orientation = TOP_DOWN_Q
 
             self.get_logger().info("Computing IK for PLACE HOVER pose (over bin)...")
@@ -244,7 +475,7 @@ class PickPlaceNode(Node):
             place_pose.header.frame_id = "world"
             place_pose.pose.position.x = 0.5
             place_pose.pose.position.y = -0.3
-            place_pose.pose.position.z = 0.40   # a bit above bin top (0.30)
+            place_pose.pose.position.z = 0.32   # a bit above bin top (0.30)
             place_pose.pose.orientation = TOP_DOWN_Q
 
             self.get_logger().info("Computing IK for PLACE pose (bin)...")
@@ -266,6 +497,7 @@ class PickPlaceNode(Node):
 
         finally:
             self.busy = False
+    """
 
 
     # ------------------------------------------------------------------
@@ -280,6 +512,7 @@ class PickPlaceNode(Node):
         req.ik_request.pose_stamped = pose_stamped
 
         self.get_logger().info("Calling /compute_ik...")
+        ik_start = time.time()
         future = self.ik_client.call_async(req)
 
         # Wait up to 5 seconds
@@ -291,6 +524,10 @@ class PickPlaceNode(Node):
 
         resp = future.result()
         self.get_logger().info(f"IK result code: {resp.error_code.val}")
+        
+        ik_time = time.time() - ik_start
+        self.metrics["ik_time"] = ik_time
+        self.get_logger().info(f"IK solve time: {ik_time:.4f} seconds")
 
         if resp.error_code.val != 1:
             self.get_logger().error("IK failed.")
@@ -354,6 +591,7 @@ class PickPlaceNode(Node):
         req.motion_plan_request = mreq
 
         self.get_logger().info("Calling /plan_kinematic_path...")
+        plan_start = time.time()
         future = self.plan_client.call_async(req)
         rclpy.spin_until_future_complete(self, future, timeout_sec=10.0)
 
@@ -368,6 +606,18 @@ class PickPlaceNode(Node):
 
         code = resp.motion_plan_response.error_code.val
         self.get_logger().info(f"Planning result code: {code}")
+        
+        plan_time = time.time() - plan_start
+        # auto-assign based on label passed into run_pick_place()
+        if "hover" in self.current_phase:
+            self.metrics["plan_hover"] = plan_time
+        elif "grasp" in self.current_phase:
+            self.metrics["plan_grasp"] = plan_time
+        elif "lift" in self.current_phase:
+            self.metrics["plan_lift"] = plan_time
+        elif "place" in self.current_phase:
+            self.metrics["plan_place"] = plan_time
+        self.get_logger().info(f"Planning time: {plan_time:.4f} seconds")
 
         if code != MoveItErrorCodes.SUCCESS:
             self.get_logger().error("Planning failed with non-success error code.")
@@ -431,6 +681,7 @@ class PickPlaceNode(Node):
         goal.trajectory = plan_resp.motion_plan_response.trajectory
 
         self.get_logger().info(f"Sending trajectory for execution: {label}")
+        exec_start = time.time()
         send_future = self.execute_client.send_goal_async(goal)
         rclpy.spin_until_future_complete(self, send_future, timeout_sec=10.0)
         if not send_future.done():
@@ -454,6 +705,16 @@ class PickPlaceNode(Node):
         self.get_logger().info(
             f"Execution finished for {label} (MoveIt error_code={error_val})"
         )
+        exec_time = time.time() - exec_start
+        if "hover" in label:
+            self.metrics["exec_hover"] = exec_time
+        elif "grasp" in label:
+            self.metrics["exec_grasp"] = exec_time
+        elif "lift" in label:
+            self.metrics["exec_lift"] = exec_time
+        elif "place" in label:
+            self.metrics["exec_place"] = exec_time
+        self.get_logger().info(f"Trajectory execution time: {exec_time:.4f} seconds")
         return error_val == 1
         
     def close_gripper(self):
